13a4b2414de27829cfb9d7da5baabbffb37d221b
ruslandoga <67764432+ruslandoga@users.noreply.github.com>
2022-06-03 14:14:34 +0300

    colors

--- lib/w2_web/live/dashboard/index.ex
+++ lib/w2_web/live/dashboard/index.ex
@@ -12,22 +12,7 @@ defmodule W2Web.DashboardLive.Index do
     <div class="min-h-screen w-full bg-red-100 flex flex-col md:flex-row font-mono">
       <div class="md:w-1/2 lg:w-3/4 bg-red-200 flex flex-col order-2 md:order-1">
         <div class="px-4 pt-4 pb-2 h-64 md:h-1/2">
-          <div class="relative h-full bg-red-900">
-            <%# buckets: <%= Jason.encode!(@buckets) %>
-            <%
-              to = DateTime.from_naive!(@to || NaiveDateTime.utc_now(), "Etc/UTC")
-              from = DateTime.from_naive!(@from || add_days(to, -4), "Etc/UTC")
-              to = DateTime.to_unix(to)
-              from = DateTime.to_unix(from)
-              interval = Durations.interval(from, to)
-              range = to - from + interval
-              width = interval / range
-            %>
-            <%= for [time, totals] <- @buckets do %>
-              <% height = Enum.reduce(totals, 0, fn {_project, total}, acc -> acc + total end) %>
-              <div class="absolute bg-red-400" style={"bottom:0;left:#{(time - from) / range * 100}%;width:#{width * 100}%;height:#{height / interval * 100}%;"}></div>
-            <% end %>
-          </div>
+          <.bar_chart from={@from} to={@to} buckets={@buckets} colors={@colors} />
         </div>
         <div class="px-4 pb-4 pt-2 md:h-1/2">
           <div class="relative bg-red-800 h-full">
@@ -57,28 +42,81 @@ defmodule W2Web.DashboardLive.Index do
           Total <%= format_time(@total) %>
         </div>
         <div class="px-4 pb-4">
-          <table class="border w-full border-red-700">
-            <thead class="border border-red-700">
-              <th class="px-1 text-left">project</th>
-              <th class="px-1 text-left">time</th>
-            </thead>
-            <tbody class="divide-y divide-red-700">
-              <%= for {project, total} <- @projects do %>
-                <tr>
-                  <td class="px-1 font-medium hover:text-red-500 cursor-pointer text-ellipsis"><%= project %></td>
-                  <td class="font-medium"><%= format_time(total) %></td>
-                </tr>
-              <% end %>
-            </tbody>
-          </table>
+          <.table projects={@projects} colors={@colors} />
         </div>
       </div>
     </div>
     """
   end
 
-  # defp color(project) do
-  # end
+  defp bar_chart(assigns) do
+    to = DateTime.from_naive!(assigns.to || NaiveDateTime.utc_now(), "Etc/UTC")
+    from = DateTime.from_naive!(assigns.from || add_days(to, -4), "Etc/UTC")
+    to = DateTime.to_unix(to)
+    from = DateTime.to_unix(from)
+    interval = Durations.interval(from, to)
+    range = to - from + interval
+    width = interval / range
+
+    assigns =
+      assign(assigns,
+        from: from,
+        range: range,
+        width: Float.round(width * 100, 4),
+        max_height: interval
+      )
+
+    ~H"""
+    <div class="relative h-full bg-red-900">
+      <%= for [time, totals] <- @buckets do %><.bar totals={totals} x={Float.round((time - @from) / @range * 100, 4)} width={@width} max_height={@max_height} colors={@colors} /><% end %>
+    </div>
+    """
+  end
+
+  defp bar(assigns) do
+    %{colors: colors, max_height: max_height} = assigns
+
+    {_, heights} =
+      Enum.reduce(assigns.totals, {0, []}, fn {project, total}, {acc, heights} ->
+        {acc + total,
+         [
+           {Float.round(acc / max_height * 100, 2), Float.round(total / max_height * 100, 2),
+            colors[project]}
+           | heights
+         ]}
+      end)
+
+    assigns = assign(assigns, heights: heights)
+
+    ~H"""
+    <%= for {y, height, color} <- @heights do %>
+      <div class={"absolute #{color}"} style={"bottom:#{y}%;left:#{@x}%;width:#{@width}%;height:#{height}%;"}></div>
+    <% end %>
+    """
+  end
+
+  defp table(assigns) do
+    ~H"""
+    <table class="border w-full border-red-700">
+      <thead class="border border-red-700">
+        <th class="px-1 text-left">project</th>
+        <th class="px-1 text-left">time</th>
+      </thead>
+      <tbody class="divide-y divide-red-700">
+        <%= for {project, total} <- @projects do %><.table_row project={project} color={@colors[project]} total={total} /><% end %>
+      </tbody>
+    </table>
+    """
+  end
+
+  defp table_row(assigns) do
+    ~H"""
+    <tr>
+      <td class={"px-1 font-medium #{@color} hover:opacity-80 cursor-pointer text-ellipsis"}><%= @project %></td>
+      <td class="font-medium"><%= format_time(@total) %></td>
+    </tr>
+    """
+  end
 
   @impl true
   def mount(_params, _session, socket) do
@@ -120,15 +158,32 @@ defmodule W2Web.DashboardLive.Index do
       # TODO
       |> Enum.sort_by(fn {_project, time} -> time end, :desc)
 
+    # TODO
+    project_colors = Map.new(projects_data, fn {project, _time} -> {project, color(project)} end)
     timeline_data = Durations.timeline_data(from, to)
 
     socket
     |> assign(total: total_data)
-    |> assign(projects: projects_data)
+    |> assign(projects: projects_data, colors: project_colors)
     |> assign(buckets: bucket_data)
     # |> push_event("bucket", %{"data" => bucket_data})
     # |> push_event("timeline", %{"data" => timeline_data})
     |> assign(timeline: timeline_data)
+  end
+
+  @colors [
+    "bg-green-400",
+    "bg-red-400",
+    "bg-blue-400",
+    "bg-cyan-500",
+    "bg-pink-500",
+    "bg-sky-400"
+  ]
+
+  @colors_count length(@colors)
+
+  defp color(project) do
+    Enum.at(@colors, :erlang.phash2(project, @colors_count))
   end
 
   @spec add_days(NaiveDateTime.t(), integer) :: NaiveDateTime.t()
