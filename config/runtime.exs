197036fee047e9daadca87d098361d133bf59bc8
ruslandoga <67764432+ruslandoga@users.noreply.github.com>
2022-06-08 23:19:20 +0300

    cont

--- config/runtime.exs
+++ config/runtime.exs
@@ -110,7 +110,8 @@ end
 
 if config_env() == :dev do
   config :w2, api_key: "406fe41f-6d69-4183-a4cc-121e0c524c2b"
-  config :w2, W2.Repo, database: Path.expand("../w2_dev.db", Path.dirname(__ENV__.file))
+  database = System.get_env("DATABASE") || "w2_dev.db"
+  config :w2, W2.Repo, database: Path.expand("../" <> database, Path.dirname(__ENV__.file))
 end
 
 if config_env() == :test do
