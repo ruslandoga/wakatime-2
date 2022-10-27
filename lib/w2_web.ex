fa007fc9d6e0722bf6b94ece5e7b98a1014ecf38
ruslandoga <67764432+ruslandoga@users.noreply.github.com>
2022-10-27 15:18:08 +0700

    fix lv warning

--- lib/w2_web.ex
+++ lib/w2_web.ex
@@ -45,7 +45,7 @@ defmodule W2Web do
   def live_view do
     quote do
       use Phoenix.LiveView,
-        layout: {W2Web.LayoutView, "live.html"}
+        layout: {W2Web.LayoutView, :live}
 
       unquote(view_helpers())
     end
