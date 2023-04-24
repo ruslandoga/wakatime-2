681f0afb3fd1929f2ce972653a7b41b8955993ca
ruslandoga <67764432+ruslandoga@users.noreply.github.com>
2023-04-25 01:49:17 +0700

    update elixir/erlang/alpine versions

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule W2.MixProject do
     [
       app: :w2,
       version: "0.1.0",
-      elixir: "~> 1.12",
+      elixir: "~> 1.14",
       elixirc_paths: elixirc_paths(Mix.env()),
       start_permanent: Mix.env() == :prod,
       aliases: aliases(),
